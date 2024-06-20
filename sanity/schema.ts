55197f3fa7baca6fa501cf6937d0b74aa25c9573
MBarni <mbarni458@gmail.com>
2024-06-20 14:05:41 +0200

    event schema added

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 
 import member from './schemaTypes/member'
 import lecturer from './schemaTypes/lecturer'
+import event from './schemaTypes/event'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [member, lecturer],
+  types: [member, lecturer, event],
 }
