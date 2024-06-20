72c27674a48ff61b3136b8560bf982fbdeac909d
MBarni <mbarni458@gmail.com>
2024-06-20 13:38:05 +0200

    lecturer schema added

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 import member from './schemaTypes/member'
+import lecturer from './schemaTypes/lecturer'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [member],
+  types: [member, lecturer],
 }
---
+++ sanity/schemaTypes/lecturer.ts
@@ -0,0 +1,34 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'lecturer',
+  title: 'Lecturer',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+        },
+      ],
+    }),
+  ],
+})
