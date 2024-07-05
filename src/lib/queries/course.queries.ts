cd285e1e7f0c179b988af47e5ef1cda9ab261cc0
MBarni <mbarni458@gmail.com>
2024-07-05 19:52:31 +0200

    remove unused files

--- src/lib/queries/course.queries.ts
+++
@@ -1,8 +0,0 @@
-import { SanityClient, groq } from 'next-sanity'
-import { Course } from '../sanity.types'
-
-export const coursesQuery = groq`*[_type == "course"]`
-
-export async function getCourses(client: SanityClient): Promise<Course[]> {
-  return await client.fetch(coursesQuery)
-}
