82718ec21cdbae2e866265d9120ea49f0a37eaf0
MBarni <mbarni458@gmail.com>
2024-07-15 16:46:51 +0200

    Count up animation

--- src/components/partners-components/PartnersSection.tsx
+++ src/components/partners-components/PartnersSection.tsx
@@ -1,15 +1,36 @@
 import { Partner } from '~/lib/sanity.types'
 import { PartnerLogo } from '~/components/partners-components/PartnerLogo'
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor'
+import { useState } from 'react'
 
 interface Props {
   partners: Partner[]
 }
 
 export const PartnersSection = ({ partners }: Props) => {
+  const [counted, setCounted] = useState(false)
+
   return (
     <div>
       <h1 className="text-center text-4xl font-bold py-5">
-        Partnereink: {partners.length}
+        Partnereink:
+        <CountUp start={0} end={partners.length}>
+          {({ countUpRef, start }) => (
+            <div>
+              <VisibilitySensor
+                onChange={(isVisible) => {
+                  if (!counted && isVisible) {
+                    setCounted(true)
+                    start()
+                  }
+                }}
+              >
+                <span ref={countUpRef} />
+              </VisibilitySensor>
+            </div>
+          )}
+        </CountUp>
       </h1>
       <div className="flex items-center">
         {partners.map((partner) => (
