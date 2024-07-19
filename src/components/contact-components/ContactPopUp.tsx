ebaf7c0d0dd5701d74a11c87beaac122360f9a2f
MBarni <mbarni458@gmail.com>
2024-07-19 15:10:57 +0200

    larger bottom padding at small size

--- src/components/contact-components/ContactPopUp.tsx
+++ src/components/contact-components/ContactPopUp.tsx
@@ -42,7 +42,7 @@ export const ContactPopUp = () => {
           className={`w-screen h-screen bg-black fixed inset-0 z-50 transition ease-out duration-300 ${isOpen ? 'bg-opacity-80' : 'bg-opacity-0'}`}
         >
           <div
-            className={`w-full md:w-3/4 lg:w-1/2 max-h-screen h-auto bg-white fixed inset-x-0 mx-auto rounded-lg py-8 my-5 shadow-md text-white transition-all duration-500 overflow-y-auto ${isOpen ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}
+            className={`w-full md:w-3/4 lg:w-1/2 max-h-screen h-auto bg-white fixed inset-x-0 mx-auto rounded-lg py-8 pb-32  md:pb-8 my-5 shadow-md text-white transition-all duration-500 overflow-y-auto ${isOpen ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}
           >
             <div
               className={`transition-opacity ${isSubmitted ? 'opacity-100' : 'opacity-0'}`}
