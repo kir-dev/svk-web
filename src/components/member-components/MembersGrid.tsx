117436b69133b7beb8f572114f8f02c3b6c5ed99
MBarni <mbarni458@gmail.com>
2024-08-29 16:20:48 +0200

    grid cols fixed

--- src/components/member-components/MembersGrid.tsx
+++ src/components/member-components/MembersGrid.tsx
@@ -1,6 +1,6 @@
 export const MembersGrid = ({ children }) => {
   return (
-    <div className="grid grid-cols-1  sm:grid-cols-gap  lg:grid-cols-3 xl:grid-cols-4 gap-5 w-fit mx-auto justify-center">
+    <div className="grid grid-cols-1  sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-5 w-fit mx-auto justify-center">
       {children}
     </div>
   )
