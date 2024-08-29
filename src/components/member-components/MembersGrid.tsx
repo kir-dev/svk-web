4c3f153a35c1acf2f8aa73cea78637a2d610625d
MBarni <mbarni458@gmail.com>
2024-08-29 11:28:44 +0200

    gaps in the grid fixed

--- src/components/member-components/MembersGrid.tsx
+++ src/components/member-components/MembersGrid.tsx
@@ -1,6 +1,6 @@
 export const MembersGrid = ({ children }) => {
   return (
-    <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-rows-3 xl:grid-cols-4 gap-5 w-full md:w-5/6 mx-auto px-3 md:px-5 justify-center">
+    <div className="grid grid-cols-1  sm:grid-cols-gap  lg:grid-cols-3 xl:grid-cols-4 gap-5 w-fit mx-auto justify-center">
       {children}
     </div>
   )
