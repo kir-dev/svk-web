cf559675a380e2eba5f117eaf135a5b0fa7c414e
MBarni <mbarni458@gmail.com>
2024-09-02 20:36:05 +0200

    navbar base created

--- src/components/navbar/NavbarSitewide.tsx
+++ src/components/navbar/NavbarSitewide.tsx
@@ -1,35 +1,16 @@
 'use client'
 
-import {
-  ChevronDownIcon,
-  ChevronRightIcon,
-  MoonIcon,
-  SunIcon,
-} from '@heroicons/react/24/solid'
-import {
-  Button,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownTrigger,
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenu,
-  NavbarMenuItem,
-  NavbarMenuToggle,
-} from '@nextui-org/react'
 import { useTheme } from 'next-themes'
-import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import { FC, PropsWithChildren, useEffect, useState } from 'react'
 
 import { Route } from '~/utils/routes'
 
 import { useTranslations } from 'next-intl'
-import { LogoAnimated } from '../svg-components/LogoAnimated'
 import { ContactPopUp } from '~/components/contact-components/ContactPopUp'
+import { MailIconSvg } from '~/components/svg-components/MailIconSvg'
+import NextLink from 'next/link'
+import { Button } from '@nextui-org/react'
 
 export interface Props {
   routes: Route[]
@@ -53,64 +34,42 @@ export const NavbarSitewide: FC<PropsWithChildren<Props>> = ({ routes }) => {
     router.push({ pathname, query }, asPath, { locale: nextLocale })
   }
 
-  return (
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? t('navbar.closeMenu') : t('navbar.openMenu')}
-          className="sm:hidden"
-        />
-        <NavbarBrand>
-          <NextLink aria-label={t('navbar.home')} href="/">
-            <LogoAnimated style={{ height: '1rem' }} />
-          </NextLink>
-        </NavbarBrand>
-      </NavbarContent>
+  const [openDropdown, setOpenDropdown] = useState<boolean>(false)
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {routes.map((route) =>
-          route.routes?.length ? (
-            <Dropdown key={route.key}>
-              <NavbarItem>
-                <DropdownTrigger>
-                  <Button
-                    disableRipple
-                    className="p-0 bg-transparent data-[hover=true]:bg-transparent text-md"
-                    endContent={<ChevronDownIcon className="h-3 w-3" />}
-                    radius="sm"
-                    variant="light"
-                  >
-                    {t(`routes.${route.key}`)}
-                  </Button>
-                </DropdownTrigger>
-              </NavbarItem>
-              <DropdownMenu
-                aria-label={t(`routes.${route.key}`)}
-                className="w-[340px]"
-                itemClasses={{
-                  base: 'gap-4',
-                }}
-              >
-                {route.routes.map((subRoute) => (
-                  <DropdownItem
-                    key={subRoute.key}
-                    startContent={
-                      subRoute.icon ? (
-                        <subRoute.icon className="h-4 w-4" />
-                      ) : (
-                        <ChevronRightIcon className="h-4 w-4" />
-                      )
-                    }
-                    onClick={() => router.push(subRoute.href ?? '')}
-                  >
-                    {t(`routes.${subRoute.key}`)}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-          ) : (
-            <NavbarItem key={route.key}>
+  return (
+    <nav className="sticky top-0">
+      <div className="flex justify-between w-screen px-5 md:px-20 py-3 bg-blue-950">
+        <div className="w-fit">
+          <ContactPopUp>
+            <MailIconSvg />
+          </ContactPopUp>
+        </div>
+        <div className="w-fit flex justify-around gap-10 items-center">
+          <div className="block md:hidden h-fit">
+            <Button onClick={() => setOpenDropdown(!openDropdown)}>
+              Dropdwon
+            </Button>
+            <div
+              className={`${openDropdown ? 'block' : 'hidden'} absolute -bottom-15 bg-black p-5 rounded`}
+            >
+              {routes.map((route) => (
+                <NextLink
+                  key={route.key}
+                  color="foreground"
+                  className={` ${route.href == pathname ? 'underline' : ''} text-md block mb-3`}
+                  href={route.href ?? ''}
+                  aria-label={t(`routes.${route.key}`)}
+                >
+                  {t(`routes.${route.key}`)}
+                </NextLink>
+              ))}
+            </div>
+          </div>
+
+          <div className="hidden md:flex justify-around gap-10">
+            {routes.map((route) => (
               <NextLink
+                key={route.key}
                 color="foreground"
                 className="text-md"
                 href={route.href ?? ''}
@@ -118,70 +77,30 @@ export const NavbarSitewide: FC<PropsWithChildren<Props>> = ({ routes }) => {
               >
                 {t(`routes.${route.key}`)}
               </NextLink>
-            </NavbarItem>
-          ),
-        )}
-      </NavbarContent>
+            ))}
+          </div>
 
-      <NavbarContent className="flex gap-2" justify="end">
-        <NavbarItem className="flex h-full items-center">
-          <ContactPopUp>
-            <h1 className="rounded-full bg-teal-300 text-white p-3">
-              {t('navbar.buttonTitle')}
-            </h1>
-          </ContactPopUp>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="flex gap-2" justify="end">
-        <NavbarItem className="flex h-full items-center">
-          <Button
-            isIconOnly
-            aria-label={t('navbar.langSwitcher')}
-            className="p-0"
-            variant="flat"
-            onPress={switchLocale}
-          >
-            {locale === 'en' ? 'HU' : 'EN'}
-          </Button>
-        </NavbarItem>
-        <NavbarItem className="flex h-full items-center">
-          <Button
-            isIconOnly
-            aria-label={t('navbar.themeSwitcher')}
-            className="p-0"
-            variant="flat"
-            onPress={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-          >
-            {theme === 'dark' ? (
-              <SunIcon className="h-5 w-5" />
-            ) : (
-              <MoonIcon className="h-5 w-5" />
-            )}
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarMenu className="flex gap-4 pt-4 bg-background bg-opacity-70">
-        {routes
-          .flatMap((item) => (item.routes?.length ? item.routes : item))
-          .map((item) => (
-            <NavbarMenuItem key={item.key} className="flex items-center gap-3">
-              {item.icon ? (
-                <item.icon className="h-5 w-5" />
-              ) : (
-                <ChevronRightIcon className="h-5 w-5" />
-              )}
-              <NextLink
-                className="w-full text-lg"
-                color="foreground"
-                href={item.href ?? ''}
-              >
-                {t(`routes.${item.key}`)}
-              </NextLink>
-            </NavbarMenuItem>
-          ))}
-      </NavbarMenu>
-    </Navbar>
+          <div>
+            <h1>Csatlakozz</h1>
+          </div>
+          <div>
+            <ContactPopUp>
+              <h1 className="bg-blue-500 rounded-full p-3">Kapcsolatok</h1>
+            </ContactPopUp>
+          </div>
+          <div>
+            <Button
+              isIconOnly
+              aria-label={t('navbar.langSwitcher')}
+              className="p-0"
+              variant="flat"
+              onPress={switchLocale}
+            >
+              {locale === 'en' ? 'HU' : 'EN'}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
