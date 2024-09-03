fe47149cf667bdd7651831288fe5b8b22209e642
MBarni <mbarni458@gmail.com>
2024-09-03 21:46:01 +0200

    layout created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,10 +31,23 @@ export default function IndexPage(
 
   return (
     <Layout>
-      <section className="flex flex-col items-center h-[90vh] sm:h-[96vh] justify-center px-6 sm:px-0 pb-8">
-        <h1 className="mb-6 text-4xl font-extrabold leading-none tracking-tight">
-          {t('mainTitle')}
-        </h1>
+      <section className=" items-center h-[90vh] sm:h-[96vh] justify-center px-6 sm:px-0 pb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 w-5/6 items-center mx-auto my-16 lg:my-56">
+          <div className={' justify-center'}>
+            <h1 className="mb-6 text-md lg:text-2xl font-extrabold leading-none tracking-tight">
+              {t('mainTitle')}
+            </h1>
+            <h1 className="mb-6 text-3xl lg:text-5xl font-extrabold leading-none tracking-tight">
+              {t('motto')}
+            </h1>
+          </div>
+          <div className={'w-full justify-center'}>
+            <img
+              src="https://imgs.search.brave.com/nyh3wzw-YJg-Nl8sZEz0T1-qpsr-RLjJGTiLaFP5ryE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5saWNkbi5jb20v/ZG1zL2ltYWdlL0M1/MTFCQVFINDRhaUly/aXU0ZUEvY29tcGFu/eS1iYWNrZ3JvdW5k/XzEwMDAwLzAvMTU4/NDQyMDAyMDQ1Ni9z/dmtfdGVjaG5vbG9n/eV9zb2x1dGlvbnNf/aW5jX2NvdmVyP2U9/MjE0NzQ4MzY0NyZ2/PWJldGEmdD04NFRy/TmE3c2NBSkMtZTZY/VFB0YlowaDJZTlBJ/OWtBRkJnS2dZLWw1/WFFV"
+              className="rounded-xl mx-auto"
+            ></img>
+          </div>
+        </div>
       </section>
       <section className="bg-gradient-to-r from-foreground-50 to-foreground-200 border-gray-300 border-y-1 py-24">
         <Container className="relative">Carousel</Container>
