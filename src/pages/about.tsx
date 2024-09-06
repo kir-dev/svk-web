ffd8b07e01102b4703d76db518cc7d0123c31be2
balog-b <bors.balog@gmail.com>
2024-09-06 16:04:24 +0200

    added about sections

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,6 +7,7 @@ import { MemberCard } from '~/components/member-components/MemberCard'
 import { MembersGrid } from '~/components/member-components/MembersGrid'
 import { useEffect, useState } from 'react'
 import { useTranslations } from 'next-intl'
+import Image from 'next/image'
 
 export const getStaticProps = async ({ locale }) => {
   const client = getClient()
@@ -19,6 +20,8 @@ export const getStaticProps = async ({ locale }) => {
   }
 }
 
+
+
 export default function AboutUsPage(
   props: InferGetStaticPropsType<typeof getStaticProps>,
 ) {
@@ -29,35 +32,39 @@ export default function AboutUsPage(
   const [ textGap, setTextGap ] = useState<string>('96')
   const [ floatInOpacity, setFloatInOpacity ] = useState<string>('0')
 
-  const [ counter, setCounter ] = useState<number>(0)
+
+  const sections = [
+    {key:"0", text:t(`about0`), href: "/images/people.avif", altHref: "Our Team"},
+    {key:"1", text:t(`about1`), href: "/images/board.avif", altHref: "What we achieve"},
+    {key:"2", text:t(`about2`), href: "/images/bullseye.avif", altHref: "Goal"},
+  ]
 
   useEffect(() => {
-    setTextGap(`0`);
+    setTextGap(`1`);
     setFloatInOpacity(`100`);
-    setTimeout( () => { setCounter( counter + 1 ); console.log(counter)}, 1000 )
-  }, [counter]);
+  }, []);
 
   return (
     <Layout>
       <section>
         <div className="flex flex-wrap relative">
           <div className="justify-center font-bold text-[#BDBFC3] w-full md:w-1/3 my-auto overflow-hidden">
-            <div className="w-fit mx-auto p-8 md:p-0">
-              <p style={{ transitionProperty: `all`, transitionDuration: `2s`, letterSpacing: `${textGap}px` }} ><span className="text-white text-5xl">S</span><span className="text-2xl md:text-3xl transition" style={{ transitionDuration: `2s`, opacity: `${floatInOpacity}` }} >chönherz</span></p>
-              <p style={{ transitionProperty: `all`, transitionDuration: `2s`, transitionDelay: `0.25s`, letterSpacing: `${textGap}px` }}><span className="text-white text-5xl">V</span><span className="text-2xl md:text-3xl transition" style={{ transitionProperty: `all`, transitionDuration: `2s`, transitionDelay: `0.25s`, opacity: `${floatInOpacity}` }} >állalati</span></p>
-              <p style={{ transitionProperty: `all`, transitionDuration: `2s`, transitionDelay: `0.5s`, letterSpacing: `${textGap}px` }}><span className="text-white text-5xl">K</span><span className="text-2xl md:text-3xl transition" style={{ transitionProperty: `all`, transitionDuration: `2s`, transitionDelay: `0.5s`, opacity: `${floatInOpacity}` }} >apcsolatok</span></p>
+            <div className="w-fit md:ml-14 lg:ml-32 xl:ml-56 2xl:ml-72 p-8 md:p-0">
+              <p style={{ transitionProperty: `all`, transitionDuration: `2s`, letterSpacing: `${textGap}px` }} ><span className="text-white text-5xl">S</span><span className="text-3xl" style={{ transitionProperty: `all`, transitionDuration: `2s`, opacity: `${floatInOpacity}` }} >chönherz</span></p>
+              <p style={{ transitionProperty: `all`, transitionDuration: `2s`, transitionDelay: `0.25s`, letterSpacing: `${textGap}px` }}><span className="text-white text-5xl">V</span><span className="text-3xl" style={{ transitionProperty: `all`, transitionDuration: `2s`, transitionDelay: `0.25s`, opacity: `${floatInOpacity}` }} >állalati</span></p>
+              <p style={{ transitionProperty: `all`, transitionDuration: `2s`, transitionDelay: `0.5s`, letterSpacing: `${textGap}px` }}><span className="text-white text-5xl">K</span><span className="text-3xl" style={{ transitionProperty: `all`, transitionDuration: `2s`, transitionDelay: `0.5s`, opacity: `${floatInOpacity}` }} >apcsolatok</span></p>
             </div>
           </div>
+
           <div className="flex flex-col gap-5 w-full h-fit md:w-2/3 justify-center p-8">
-            <div className="bg-black bg-opacity-50 rounded-full p-4">
-              {t(`about0`)}
-            </div>
-            <div className="bg-black bg-opacity-50 rounded-full p-4">
-              {t(`about1`)}
-            </div>
-            <div className="bg-black bg-opacity-50 rounded-full p-4">
-              {t(`about2`)}
-            </div>
+              {sections.map((section) => (
+                <div key={section.key} className="flex bg-black bg-opacity-50 rounded-[100px] lg:rounded-full p-5 gap-4">
+                  <Image src={section.href} alt={section.altHref} width="80" height="80" className="h-full my-auto hidden sm:block" />
+                  <p className="my-auto pl-10 pr-10 sm:pl-0 text-justify text-medium sm:text-lg basis-[100%] sm:basis-[90%] sm:leading-loose">
+                    {section.text}
+                  </p>
+                </div>
+              ))}
           </div>
         </div>
       </section>
