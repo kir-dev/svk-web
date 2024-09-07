06f1bb9fb19f7a00597914392ed6de54a31d7087
MBarni <mbarni458@gmail.com>
2024-09-07 15:34:38 +0200

    You spin me right 'round, baby

--- src/components/carousel-components/Carousel.tsx
+++ src/components/carousel-components/Carousel.tsx
@@ -8,17 +8,25 @@ export const Carousel: React.FC<Props> = ({ children }: Props) => {
   const [index, setIndex] = useState<number>(0)
 
   useEffect(() => {
+    children[children.length] = children[0]
+  }, [children])
+
+  useEffect(() => {
+    let time = 5000
+    if (index == 0) {
+      time = 100
+    }
     setTimeout(() => {
       setIndex(() => (index === children.length - 1 ? 0 : index + 1))
-    }, 5000)
-  }, [index, children.length])
+    }, time)
+  }, [children.length, index])
   return (
     <div
       className={'w-full overflow-hidden rounded-xl justify-start aspect-video'}
     >
       <div
         style={{ transform: `translateX(${index * -100}%)` }}
-        className={`flex flex-nowrap transition-all`}
+        className={`flex flex-nowrap ${index == 0 ? 'transition-none' : 'transition-all'}`}
       >
         {...children}
       </div>
