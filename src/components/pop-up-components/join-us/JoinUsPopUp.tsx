09607478c56e7529dce902f2873d0e80a453d7f5
MBarni <mbarni458@gmail.com>
2024-09-17 12:07:21 +0200

    join us form can be open by url

--- src/components/pop-up-components/join-us/JoinUsPopUp.tsx
+++ src/components/pop-up-components/join-us/JoinUsPopUp.tsx
@@ -1,13 +1,26 @@
 import { PopUp } from '~/components/pop-up-components/PopUp'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { JoinUsFrom } from '~/components/pop-up-components/join-us/JoinUsForm'
 import { useTranslations } from 'next-intl'
+import { useSearchParams } from 'next/navigation'
 
 interface Props {
   children: React.ReactNode
 }
 
 export const JoinUsPopUp = ({ children }: Props) => {
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    if (!searchParams || typeof window === 'undefined') {
+      return
+    }
+    const modalParams = searchParams.get('modal')
+    if (!isOpen && modalParams == 'joinus') {
+      setIsOpen(true)
+    }
+  }, [searchParams])
+
   const t = useTranslations('common.joinUs')
   const [isOpen, setIsOpen] = useState(false)
   return (
