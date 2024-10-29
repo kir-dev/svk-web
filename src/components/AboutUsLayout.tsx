47e1fb6bfbe8512068bf26a384ebe76e038f2d79
balog-b <bors.balog@gmail.com>
2024-10-29 17:08:32 +0100

    fix: responsiveness

--- src/components/AboutUsLayout.tsx
+++ src/components/AboutUsLayout.tsx
@@ -6,11 +6,12 @@ import { Blur } from '~/components/decoration-components/Blur'
 import Image from 'next/image'
 
 export default function AboutUsLayout({ children }) {
+
   return (
     <div className="z-20 relative">
       <NavbarSitewide routes={allRoutes} />
 
-      <div className="h-[50vh] relative">
+      <div className={`w-screen aspect-[3/2] sm:aspect-[4/2] md:aspect-[5/2] lg:aspect-[3/1] xl:aspect-[4/1] relative` }>
         <Image src="/images/SVK_group.jpg" alt="SVK" width={0} height={0} sizes="100vw"
                className="w-screen fixed overflow-y-hidden"/>
       </div>
