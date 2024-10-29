403be6239c5d3515b05723b0beef34a64c9403d5
balog-b <bors.balog@gmail.com>
2024-10-29 16:07:40 +0100

    feat: about us picture

---
+++ src/components/AboutUsLayout.tsx
@@ -0,0 +1,30 @@
+import { allRoutes } from '~/utils/routes'
+
+import { Footer } from './Footer'
+import { NavbarSitewide } from './navbar/NavbarSitewide'
+import { Blur } from '~/components/decoration-components/Blur'
+import Image from 'next/image'
+
+export default function AboutUsLayout({ children }) {
+  return (
+    <div className="z-20 relative">
+      <NavbarSitewide routes={allRoutes} />
+
+      <div className="h-[50vh] relative">
+        <Image src="/images/SVK_group.jpg" alt="SVK" width={0} height={0} sizes="100vw"
+               className="w-screen fixed overflow-y-hidden"/>
+      </div>
+
+      <div className="d-flex flex-col bg-[#111827] z-10 relative overflow-hidden" style={{clipPath: "inset(0 0 -1px 0)"}}>
+        <Blur width={20} height={20} top={50} left={-10} />
+        <Blur width={15} height={15} top={0} left={80} />
+        <Blur width={15} height={15} top={80} left={55} />
+        <div className="relative z-10">
+          {children}
+        </div>
+      </div>
+
+      <Footer routes={allRoutes} />
+    </div>
+  )
+}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,24 +1,18 @@
-import Layout from '~/components/Layout'
 import { getMembers } from '~/lib/queries'
 import { getClient } from '~/lib/sanity.client'
 import type { InferGetStaticPropsType } from 'next'
-import { Member, Picture } from '~/lib/sanity.types'
+import { Member } from '~/lib/sanity.types'
 import { MemberCard } from '~/components/member-components/MemberCard'
 import { MembersGrid } from '~/components/member-components/MembersGrid'
 import { useTranslations } from 'next-intl'
-import { MultiCarousel } from '~/components/carousel-components/MultiCarousel'
-import { getAboutMultiCarouselImages } from '~/lib/queries/images'
-import { CarouselImage } from '~/components/carousel-components/CarouselImage'
+import AboutUsLayout from '~/components/AboutUsLayout'
 
 export const getStaticProps = async ({ locale }) => {
   const client = getClient()
   const members: Member[] = await getMembers(client)
-  const aboutMultiCarouselImages: Picture[] =
-    await getAboutMultiCarouselImages(client)
   return {
     props: {
       members: members,
-      aboutMultiCarouselImages: aboutMultiCarouselImages,
       messages: (await import(`../../messages/${locale}.json`)).default,
     },
   }
@@ -28,31 +22,24 @@ export default function AboutUsPage(
   props: InferGetStaticPropsType<typeof getStaticProps>,
 ) {
   const members = props.members
-  const aboutMultiCarouselImages = props.aboutMultiCarouselImages
 
   const t = useTranslations('Members')
 
   return (
-    <Layout>
+    <AboutUsLayout>
+
       <section>
         <div className="mx-auto h-min gap-2 w-[83%] py-24">
           <p className="text-3xl sm:text-4xl md:text-5xl text-nowrap text-center 2xl:text-start pb-4">
             {t('mission.title')}
           </p>
           <div className="text-justify flex flex-col gap-4 leading-loose text-gray-300 text-md sm:text-lg">
-              <p>{t('mission.body0')}</p>
-              <p>{t('mission.body1')}</p>
+            <p>{t('mission.body0')}</p>
+            <p>{t('mission.body1')}</p>
           </div>
         </div>
       </section>
-      <section>
-        <MultiCarousel>
-          {aboutMultiCarouselImages.map((image) => (
-            <CarouselImage key={image._id} image={image} />
-          ))}
-        </MultiCarousel>
-      </section>
-      <section>
+      <section className="pb-24">
         <MembersGrid>
           {members.map((member) => (
             <MemberCard
@@ -67,6 +54,6 @@ export default function AboutUsPage(
           ))}
         </MembersGrid>
       </section>
-    </Layout>
+    </AboutUsLayout>
   )
 }
