5be98abe08b2927d838c1b5f979858ce1d0e0194
MBarni <mbarni458@gmail.com>
2024-10-30 21:10:33 +0100

    fetch changed to axios

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,21 @@
 import { ContactFormFields } from '~/utils/form-validation'
+import axios, { AxiosResponse } from 'axios'
 
-export const sendContactFrom = async (data: ContactFormFields) =>
-  fetch('/api/contact', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-  }).then((res) => {
-    if (!res.ok) {
-      throw new Error('Failed to send contact')
-    }
-    return res.json()
-  })
+export const sendContactFrom = async (data: ContactFormFields) => {
+  try {
+    const axiosResponse: AxiosResponse = await axios.post<ContactFormFields>(
+      '/api/contact',
+      {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      },
+    )
+    return axiosResponse
+  } catch (error) {
+    return { status: 400, message: 'Contact form email submission failed!' }
+  }
+}
