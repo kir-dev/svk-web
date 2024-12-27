1aebdce4d1d3f06cb29427ff92473d0552e30f50
MBarni <mbarni458@gmail.com>
2024-12-27 17:34:38 +0100

    feat: multiple lines available

--- src/components/partners-components/PartnersSection.tsx
+++ src/components/partners-components/PartnersSection.tsx
@@ -6,12 +6,29 @@ interface Props {
   title: string
   partners: Partner[]
 }
-
 export const PartnersSection = ({ title, partners }: Props) => {
+  const partnerPerRow = 2
+
+  const splitArrays = (array: Partner[], size: number): Partner[][] => {
+    const result: Partner[][] = []
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size))
+    }
+    return result
+  }
+
+  const splitPartners: Partner[][] = splitArrays(partners, partnerPerRow)
+
   return (
     <div className="bg-gray-500 text-black py-10 overflow-x-hidden w-screen">
       <PartnersCounter title={title} length={partners.length} />
-      <PartnerRowAnimation partners={partners} toRight={true} />
+      {splitPartners.map((partner, index) => (
+        <PartnerRowAnimation
+          key={`partner${index}`}
+          partners={partner}
+          toRight={index % 2 == 0}
+        />
+      ))}
     </div>
   )
 }
