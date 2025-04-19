ddd9486e4334afe5986bc64514cdbfd53a590a49
balog-b <bors.balog@gmail.com>
2025-04-19 23:20:04 +0200

    fix: revalidate route

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -30,7 +30,7 @@ const generateEmailContent = (data: ContactFormFields) => {
   }
 }
 
-export default async function POST(req, res) {
+export async function POST(req) {
   const data: ContactFormFields = req.body
   const entries = Object.entries(data)
   const valid =
@@ -38,7 +38,12 @@ export default async function POST(req, res) {
       .map((field) => validateField(field[0], field[1]))
       .filter((valid) => valid).length == entries.length
   if (!valid) {
-    return res.status(400).send({ message: 'Incorrect form fields' })
+    return new Response(JSON.stringify({ error: 'Incorrect form fields' }), {
+      status: 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   }
   try {
     await transporter.sendMail({
@@ -46,8 +51,21 @@ export default async function POST(req, res) {
       ...generateEmailContent(data),
       subject: 'Megkeresés',
     })
-    return res.status(200).send({ message: 'Successful request' })
+
+    return new Response(JSON.stringify({ message: 'Successful request' }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   } catch (error) {
-    return res.status(400).send({ message: error.message })
+
+    const message = error?.response?.data?.message || error.message || 'Internal Server Error';
+    return new Response(JSON.stringify({ error: message }), {
+      status: error?.response?.status || 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   }
 }
