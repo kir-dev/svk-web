763d319e2250db1650dd3083983799609fed70ff
MBarni <mbarni458@gmail.com>
2025-04-28 19:43:40 +0200

    fix: form field backend validation implemented

--- src/app/api/form-submit/route.ts
+++ src/app/api/form-submit/route.ts
@@ -1,7 +1,15 @@
 import axios, { AxiosResponse } from 'axios'
+import { validateField } from '~/utils/form-validation'
 
 export async function POST(request) {
   const data = await request.json()
+  for (const field of Object.keys(data)) {
+    if (!validateField(field, data[field])) {
+      return new Response(JSON.stringify('Not every form field is valid'), {
+        status: 400,
+      })
+    }
+  }
   try {
     const axiosResponse: AxiosResponse = await axios.post(
       process.env['N8N_SHEETS_HTTP']!,
@@ -18,10 +26,10 @@ export async function POST(request) {
       headers: {
         'Content-Type': 'application/json',
       },
-    });
+    })
   } catch (error) {
-
-    const message = error?.response?.data?.message || error.message || 'Internal Server Error';
+    const message =
+      error?.response?.data?.message || error.message || 'Internal Server Error'
 
     return new Response(JSON.stringify({ error: message }), {
       status: error?.response?.status || 500,
