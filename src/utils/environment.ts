e29ec8538635c99bd72d5aa278bbda050db167ac
MBarni <mbarni458@gmail.com>
2025-08-19 19:17:03 +0200

    feat: Instagram link replaced with the real one

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   socials: {
-    instagramUrl: 'https://instagram.com/kir.dev',
+    instagramUrl: 'https://www.instagram.com/svk_bme',
     facebookUrl: 'https://www.facebook.com/SchonherzVallalatiKapcsolatok',
     linkedin: 'https://linkedin.com/company/sch%C3%B6nherz-corporate-relations',
   },
