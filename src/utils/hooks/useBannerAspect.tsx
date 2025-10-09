d88b59da6f7cbb055a54cf1ca4ec0a644d2db327
balog-b <bors.balog@gmail.com>
2025-10-09 22:11:12 +0200

    fix: functional multiplier calculation

--- src/utils/hooks/useBannerAspect.tsx
+++ src/utils/hooks/useBannerAspect.tsx
@@ -1,20 +1,25 @@
 import {useEffect, useState} from "react";
 
+type Breakpoint = {
+  min: number
+  multiplier: number
+}
+
 export function useBannerAspect(originalAspect: number) {
   const [aspect, setAspect] = useState<number>(originalAspect);
 
   useEffect(() => {
+
+    const breakpoints: Breakpoint[] = [
+      { min: 1280, multiplier: 2 },
+      { min: 1024, multiplier: 1.8 },
+      { min: 768, multiplier: 1.6 },
+      { min: 640, multiplier: 1.4 }
+    ]
+
     const handler = () => {
-      if(1280 < window.innerWidth)
-        setAspect(originalAspect * 2)
-      else if(1024 < window.innerWidth)
-        setAspect(originalAspect * 1.8)
-      else if(768 < window.innerWidth)
-        setAspect(originalAspect * 1.6)
-      else if(640 < window.innerWidth)
-        setAspect(originalAspect * 1.4)
-      else
-        setAspect(originalAspect)
+      const multiplier = breakpoints.find((bp) => bp.min < window.innerWidth)?.multiplier ?? 1
+      setAspect(originalAspect * multiplier)
     }
     window.addEventListener('resize', handler);
     handler();
