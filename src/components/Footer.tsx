5f7b53faa0b6576631852d4874cbc5276a0ae9b5
balog-b <bors.balog@gmail.com>
2025-10-09 20:19:50 +0200

    feat: slightly reworked the footer, which now includes our logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,6 +13,8 @@ import SocialIcon from '~/components/SocialIcon'
 import { MailIconSvg } from '~/components/svg-components/MailIconSvg'
 import { ContactPopUp } from '~/components/pop-up-components/contact/ContactPopUp'
 import { usePathname } from 'next/navigation'
+import KirDevWhiteSvg from "~/components/svg-components/KirDevWhiteSvg";
+import HeartSvg from "~/components/svg-components/HeartSvg";
 
 export interface Props {
   routes: Route[]
@@ -38,7 +40,7 @@ export const Footer: FC<PropsWithChildren<Props>> = ({ routes }) => {
       />
       <footer className="flex flex-col gap-2 px-[8.5%] bg-black relative z-40">
         <div className="flex justify-between gap-8 sm:gap-3 flex-col-reverse sm:flex-row my-8">
-          <div className="flex flex-col gap-5 sm:gap-2 text-center sm:text-left">
+          <div className="flex-1 flex flex-col gap-5 sm:gap-2 text-center sm:text-left">
             {routes.map((route) => (
               <NextLink
                 key={route.key}
@@ -49,20 +51,33 @@ export const Footer: FC<PropsWithChildren<Props>> = ({ routes }) => {
               </NextLink>
             ))}
           </div>
-          <div className="flex flex-col gap-5 sm:gap-2 text-center sm:text-left">
-            <p>{t('footer.svk')}</p>
-            <p>{t('footer.address0')}</p>
-            <p>{t('footer.address1')}</p>
-            <div className="flex flex-row justify-between pt-2">
-              {socials.map((item) => (
-                <SocialIcon key={item.key} href={item.href} icon={item.icon} />
-              ))}
-              <button
-                onClick={() => setIsContactModalOpen(!isContactModalOpen)}
-                className="h-auto w-auto hover:opacity-75"
-              >
-                <MailIconSvg />
-              </button>
+          <div className="flex-1">
+            <div className="mx-auto text-center">
+              Made with <HeartSvg /> by Kir-Dev
+            </div>
+            <div className="flex justify-center">
+              <a href="https://kir-dev.hu/" className="pb-4">
+                <KirDevWhiteSvg />
+              </a>
+            </div>
+
+          </div>
+          <div className="flex-1 flex flex-col gap-5 sm:gap-2 text-center sm:text-left">
+            <div className="ml-auto">
+              <p>{t('footer.svk')}</p>
+              <p>{t('footer.address0')}</p>
+              <p>{t('footer.address1')}</p>
+              <div className="flex flex-row justify-between pt-2">
+                {socials.map((item) => (
+                  <SocialIcon key={item.key} href={item.href} icon={item.icon} />
+                ))}
+                <button
+                  onClick={() => setIsContactModalOpen(!isContactModalOpen)}
+                  className="h-auto w-auto hover:opacity-75"
+                >
+                  <MailIconSvg />
+                </button>
+              </div>
             </div>
           </div>
         </div>
